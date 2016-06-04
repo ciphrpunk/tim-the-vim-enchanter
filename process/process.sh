93001c3a95389882d17b900007a09e9a88157ebc
Jimmy Ecker <jim.ecker@gmail.com>
2016-06-04 02:03:38 -0600

    Trying to get perl completions working

--- process/process.sh
+++ process/process.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 while IFS='' read -r line || [[ -n "$line" ]]; do
-  cd ../bundle; git submodule add $line ; git submodule init ; git submodule update
-done < "./plugin-list"
+  if [[ $line =~ url[[:space:]]=[[:space:]](.+) ]]
+  then
+    url=${BASH_REMATCH[1]}
+  fi
+  cd ../bundle; git submodule add $url ; git submodule init ; git submodule update ; cd ../process/
+done < "./gitmodules"
+cd ../bundle/vimproc.vim/; make 
